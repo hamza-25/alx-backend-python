b5f44d30ae84bde035fd41bfb2990301606a1b17
hamza-25 <me677499@gmail.com>
2024-03-14 18:07:06 +0000

    typeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""list module"""
+from typing import Sequence, Union, Any, Mapping, TypeVar
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None]) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
