961557deda8ebb0c819aefad6c1bea3dbf56ed47
hamza-25 <me677499@gmail.com>
2024-03-14 15:56:08 +0000

    add task 0 and 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Math module """
+
+
+def add(a: float, b: float) -> float:
+    """sum two number"""
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,5 @@
+"""string module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    return f'{str1}{str2}'
