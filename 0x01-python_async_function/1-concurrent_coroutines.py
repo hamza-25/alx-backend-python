491b3a73948f39f6d3b270c4bcbd8eb4f349210d
hamza-25 <me677499@gmail.com>
2024-03-18 15:36:22 +0000

    import from module 0-basic

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,12 +3,14 @@
 import asyncio
 import random
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """function that  return the list of all the delays (float values)"""
     delay_time: List[float] = []
     for i in range(n):
-        await asyncio.sleep(random.uniform(0, max_delay))
-        delay_time.append(random.uniform(0, max_delay))
+        time = await wait_random(max_delay)
+        await asyncio.sleep(time)
+        delay_time.append(time)
     return sorted(delay_time)
