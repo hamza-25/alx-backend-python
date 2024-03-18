7fe4afc260ebe42d93d41907b579aaa1cc8c2f34
hamza-25 <me677499@gmail.com>
2024-03-18 14:50:18 +0000

    add annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """func that print time after delay of time """
     time = random.uniform(0, max_delay)
     await asyncio.sleep(time)
