69983fadd65813ac9e91bb6fec6cd05b07f15256
hamza-25 <me677499@gmail.com>
2024-03-18 15:41:44 +0000

    update

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """function that  return the list of all the delays (float values)"""
     delay_time: List[float] = []
     for i in range(n):
-        time = await wait_random(max_delay)
+        time: float = await wait_random(max_delay)
         await asyncio.sleep(time)
         delay_time.append(time)
     return sorted(delay_time)
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Define async module"""
+import asyncio
+import random
+from typing import List
+wait_n = __import__('2-measure_runtime').wait_n
+
+async def measure_time(n: int, max_delay: int) -> float:
+    pass
+
+
