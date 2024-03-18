757ffd534ca9e81d9638efa69284fee6054c85cb
hamza-25 <me677499@gmail.com>
2024-03-18 14:46:06 +0000

    add docs

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" """
+"""Define async module"""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10):
+    """func that print time after delay of time """
     time = random.uniform(0, max_delay)
     await asyncio.sleep(time)
     return time
