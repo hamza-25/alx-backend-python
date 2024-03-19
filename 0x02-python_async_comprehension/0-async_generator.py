4e5254928c520be5f92c15131bfd14ea427f570d
hamza-25 <me677499@gmail.com>
2024-03-19 10:26:12 +0000

    add generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Define Async Module"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """function that generate random values 0 to 10"""
     for i in range(10):
         await asyncio.sleep(1)
