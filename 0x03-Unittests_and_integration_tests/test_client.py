5b5a33956c9b4668228101d6fd21bfe13301ee96
hamza-25 <me677499@gmail.com>
2024-04-06 13:58:13 +0000

    has licence

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -58,44 +58,6 @@ class TestGithubOrgClient(unittest.TestCase):
                 },
             ]
         }
-        # payload = {
-        #     'repos_url': "https://api.github.com/users/google/repos",
-        #     'repos': [
-        #         {
-        #             "id": 7697149,
-        #             "name": "episodes.dart",
-        #             "private": False,
-        #             "owner": {
-        #                 "login": "google",
-        #                 "id": 1342004,
-        #             },
-        #             "fork": False,
-        #             "url":
-        # "https://api.github.com/repos/google/episodes.dart",
-        #             "created_at": "2013-01-19T00:31:37Z",
-        #             "updated_at": "2019-09-23T11:53:58Z",
-        #             "has_issues": True,
-        #             "forks": 22,
-        #             "default_branch": "master",
-        #         },
-        #         {
-        #             "id": 8566972,
-        #             "name": "kratu",
-        #             "private": False,
-        #             "owner": {
-        #                 "login": "google",
-        #                 "id": 1342004,
-        #             },
-        #             "fork": False,
-        #             "url": "https://api.github.com/repos/google/kratu",
-        #             "created_at": "2013-03-04T22:52:33Z",
-        #             "updated_at": "2019-11-15T22:22:16Z",
-        #             "has_issues": True,
-        #             "forks": 32,
-        #             "default_branch": "master",
-        #         },
-        #     ]
-        # }
         mock_get_json.return_value = payload["repos"]
         with patch("client.GithubOrgClient._public_repos_url",
                    new_callable=PropertyMock,) as mock_public_url:
@@ -109,6 +71,18 @@ class TestGithubOrgClient(unittest.TestCase):
                              )
             mock_public_url.assert_called_once()
         mock_get_json.assert_called_once()
+
+    @parameterized.expand(
+        [
+            ({"license": {"key": "my_license"}}, "my_license", True),
+            ({"license": {"key": "other_license"}}, "other_license", False),
+        ]
+    )
+    def test_has_license(self, repo: Dict, key: str, expected: bool) -> None:
+        """method that test client has licence
+        """
+        client_has_licence = GithubOrgClient("google").has_license(repo, key)
+        self.assertEqual(client_has_licence, expected)
 
 
 if __name__ == "__main__":
