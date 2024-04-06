2559b7cd195eccc7af7b4e54006fff8355c6df29
hamza-25 <me677499@gmail.com>
2024-04-06 14:00:01 +0000

    fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -75,7 +75,7 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand(
         [
             ({"license": {"key": "my_license"}}, "my_license", True),
-            ({"license": {"key": "other_license"}}, "other_license", False),
+            ({"license": {"key": "other_license"}}, "my_license", False),
         ]
     )
     def test_has_license(self, repo: Dict, key: str, expected: bool) -> None:
