cb43669aec135d6de8fea155eaff7fc739d9132c
hamza-25 <me677499@gmail.com>
2024-04-06 13:43:53 +0000

    fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -45,56 +45,57 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_public_repos(self, mock_get_json: MagicMock) -> None:
         """test_public_repos that check repo test
         """
-        # payload = {
-        #     'repos_url': "https://api.github.com/users/google/repos",
-        #     'repos': [
-        #         {
-        #             "id": 65257748,
-        #             "name": "repo1",
-        #         },
-        #         {
-        #             "id": 1126548,
-        #             "name": "repo2",
-        #         },
-        #     ]
-        # }
         payload = {
             'repos_url': "https://api.github.com/users/google/repos",
             'repos': [
                 {
-                    "id": 7697149,
-                    "name": "episodes.dart",
-                    "private": False,
-                    "owner": {
-                        "login": "google",
-                        "id": 1342004,
-                    },
-                    "fork": False,
-                    "url": "https://api.github.com/repos/google/episodes.dart",
-                    "created_at": "2013-01-19T00:31:37Z",
-                    "updated_at": "2019-09-23T11:53:58Z",
-                    "has_issues": True,
-                    "forks": 22,
-                    "default_branch": "master",
+                    "id": 65257748,
+                    "name": "repo1",
                 },
                 {
-                    "id": 8566972,
-                    "name": "kratu",
-                    "private": False,
-                    "owner": {
-                        "login": "google",
-                        "id": 1342004,
-                    },
-                    "fork": False,
-                    "url": "https://api.github.com/repos/google/kratu",
-                    "created_at": "2013-03-04T22:52:33Z",
-                    "updated_at": "2019-11-15T22:22:16Z",
-                    "has_issues": True,
-                    "forks": 32,
-                    "default_branch": "master",
+                    "id": 1126548,
+                    "name": "repo2",
                 },
             ]
         }
+        # payload = {
+        #     'repos_url': "https://api.github.com/users/google/repos",
+        #     'repos': [
+        #         {
+        #             "id": 7697149,
+        #             "name": "episodes.dart",
+        #             "private": False,
+        #             "owner": {
+        #                 "login": "google",
+        #                 "id": 1342004,
+        #             },
+        #             "fork": False,
+        #             "url":
+        # "https://api.github.com/repos/google/episodes.dart",
+        #             "created_at": "2013-01-19T00:31:37Z",
+        #             "updated_at": "2019-09-23T11:53:58Z",
+        #             "has_issues": True,
+        #             "forks": 22,
+        #             "default_branch": "master",
+        #         },
+        #         {
+        #             "id": 8566972,
+        #             "name": "kratu",
+        #             "private": False,
+        #             "owner": {
+        #                 "login": "google",
+        #                 "id": 1342004,
+        #             },
+        #             "fork": False,
+        #             "url": "https://api.github.com/repos/google/kratu",
+        #             "created_at": "2013-03-04T22:52:33Z",
+        #             "updated_at": "2019-11-15T22:22:16Z",
+        #             "has_issues": True,
+        #             "forks": 32,
+        #             "default_branch": "master",
+        #         },
+        #     ]
+        # }
         mock_get_json.return_value = payload["repos"]
         with patch("client.GithubOrgClient._public_repos_url",
                    new_callable=PropertyMock,) as mock_public_url:
